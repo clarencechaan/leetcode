37bca5f0e7e94a1b3d1e6f26d2e0507452f3b0e1
Clarence Chan <clarencechaan@icloud.com>
2022-09-11 16:47:11 -0400

    document idea

--- medium/650.js
+++ medium/650.js
@@ -2,6 +2,9 @@
  * @param {number} n
  * @return {number}
  */
+//  1) Copy if n is divisible by the current length
+//  2) Paste
+//  3) Repeat from (1) until the current length is equal to n
 var minSteps = function (n) {
   let count = 0;
   let length = 1;
