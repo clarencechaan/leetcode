0ad7f111dd1bf50e443f8f156dd9e3cacf5eff1c
Clarence Chan <clarencechaan@icloud.com>
2022-11-15 17:23:10 -0500

    problem 2006

---
+++ easy/2006.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var countKDifference = function (nums, k) {
+  let count = 0;
+  for (let i = 0; i < nums.length - 1; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (Math.abs(nums[i] - nums[j]) === k) count++;
+    }
+  }
+  return count;
+};
+
+console.log(countKDifference([3, 2, 1, 5, 4], 2));
