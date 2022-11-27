41be84f45d46b314c5ab2da5281129d105f38019
Clarence Chan <clarencechaan@icloud.com>
2022-11-27 14:12:16 -0500

    problem 110

---
+++ easy/110.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isBalanced = function (root) {
+  function height(root) {
+    if (!root) return 0;
+    return 1 + Math.max(height(root.left), height(root.right));
+  }
+
+  if (!root) return true;
+  return (
+    Math.abs(height(root.left) - height(root.right)) <= 1 &&
+    isBalanced(root.left) &&
+    isBalanced(root.right)
+  );
+};
