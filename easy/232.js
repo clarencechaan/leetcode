f70370994474e4a6ce7b5c6f63b0901bedecae2b
Clarence Chan <clarencechaan@icloud.com>
2022-11-28 13:51:43 -0500

    problem 232

---
+++ easy/232.js
@@ -0,0 +1,41 @@
+var MyQueue = function () {
+  this.stack = [];
+};
+
+/**
+ * @param {number} x
+ * @return {void}
+ */
+MyQueue.prototype.push = function (x) {
+  this.stack.push(x);
+};
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.pop = function () {
+  return this.stack.shift();
+};
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.peek = function () {
+  return this.stack[0];
+};
+
+/**
+ * @return {boolean}
+ */
+MyQueue.prototype.empty = function () {
+  return this.stack.length === 0;
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * var obj = new MyQueue()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.peek()
+ * var param_4 = obj.empty()
+ */
