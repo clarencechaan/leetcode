4b581346162d8bd5abbd6d6ded706a3d5f5c1ed3
Clarence Chan <clarencechaan@icloud.com>
2022-12-03 15:32:52 -0500

    problem 492

--- easy/492.js
+++ easy/492.js
@@ -3,13 +3,8 @@
  * @return {number[]}
  */
 var constructRectangle = function (area) {
-  let dimensions = [];
   for (let i = 1; i <= area; i++)
-    if (area % i === 0 && i >= area / i) dimensions.push([i, area / i]);
-  return dimensions.reduce(
-    (min, d) => (d[0] - d[1] < min[0] - min[1] ? d : min),
-    dimensions[0]
-  );
+    if (area % i === 0 && i >= area / i) return [i, area / i];
 };
 
 console.log(constructRectangle(122122));
