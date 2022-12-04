d9400f686befc4eab5f31a9310e87d5a3ebb70b7
Clarence Chan <clarencechaan@icloud.com>
2022-12-04 18:10:39 -0500

    problem 617

---
+++ easy/617.js
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root1
+ * @param {TreeNode} root2
+ * @return {TreeNode}
+ */
+var mergeTrees = function (root1, root2) {
+  if (!root1 && !root2) return null;
+
+  let root = new TreeNode((root1?.val || 0) + (root2?.val || 0));
+  root.left = mergeTrees(root1?.left, root2?.left);
+  root.right = mergeTrees(root1?.right, root2?.right);
+
+  return root;
+};
