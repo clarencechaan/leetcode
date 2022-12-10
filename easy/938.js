d2ca869849ac7f8b477d0261957eecb200dd91b0
Clarence Chan <clarencechaan@icloud.com>
2022-12-10 14:55:54 -0500

    problem 938

---
+++ easy/938.js
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} low
+ * @param {number} high
+ * @return {number}
+ */
+var rangeSumBST = function (root, low, high) {
+  if (!root) return 0;
+  return (
+    (root.val >= low && root.val <= high ? root.val : 0) +
+    rangeSumBST(root.left, low, high) +
+    rangeSumBST(root.right, low, high)
+  );
+};
