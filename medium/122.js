dc8c08ccb26ed06724869c73ba127190c4756242
Clarence Chan <clarencechaan@icloud.com>
2022-12-24 14:18:07 -0500

    problem 122

---
+++ medium/122.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function (prices) {
+  let profit = 0;
+
+  for (let i = 0; i < prices.length - 1; i++)
+    if (prices[i] < prices[i + 1]) profit += prices[i + 1] - prices[i];
+
+  return profit;
+};
+
+console.log(maxProfit([7, 6, 4, 3, 1]));
