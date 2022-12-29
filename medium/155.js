275509c9e07802f5943f09505cbe2fced35b818a
Clarence Chan <clarencechaan@icloud.com>
2022-12-29 14:38:01 -0500

    problem 155

---
+++ medium/155.js
@@ -0,0 +1,44 @@
+var MinStack = function () {
+  this.stack = [];
+  this.min = [];
+};
+
+/**
+ * @param {number} val
+ * @return {void}
+ */
+MinStack.prototype.push = function (val) {
+  this.stack.push(val);
+  if (!this.min.length || val <= this.getMin()) this.min.push(val);
+};
+
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function () {
+  if (this.getMin() === this.top()) this.min.pop();
+  this.stack.pop();
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function () {
+  return this.stack[this.stack.length - 1];
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function () {
+  return this.min[this.min.length - 1];
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * var obj = new MinStack()
+ * obj.push(val)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.getMin()
+ */
