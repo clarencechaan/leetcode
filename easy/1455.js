0900b667621d73bd17867bbe857c24d9a6f15bff
Clarence Chan <clarencechaan@icloud.com>
2023-01-15 11:38:35 -0500

    problem 1455

---
+++ easy/1455.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} sentence
+ * @param {string} searchWord
+ * @return {number}
+ */
+var isPrefixOfWord = function (sentence, searchWord) {
+  let words = sentence.split(" ");
+
+  for (let i = 0; i < words.length; i++)
+    if (words[i].substring(0, searchWord.length) === searchWord) return i + 1;
+
+  return -1;
+};
