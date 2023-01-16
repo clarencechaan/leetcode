c530c045ad80b29d24a216d71924e95f609bf1fc
Clarence Chan <clarencechaan@icloud.com>
2023-01-16 12:24:45 -0500

    problem 1470

---
+++ easy/1470.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} n
+ * @return {number[]}
+ */
+var shuffle = function (nums, n) {
+  let result = [];
+  for (let i = 0; i < n; i++) {
+    result.push(nums[i]);
+    result.push(nums[n + i]);
+  }
+  return result;
+};
+
+console.log(shuffle([1, 1, 2, 2], 2));
