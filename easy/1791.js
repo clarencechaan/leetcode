f783ab5dd2700e2a2951a0cfbbee90a0bf67647b
Clarence Chan <clarencechaan@icloud.com>
2023-01-27 17:46:09 -0500

    problem 1791

---
+++ easy/1791.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[][]} edges
+ * @return {number}
+ */
+var findCenter = function (edges) {
+  if (edges[0][0] === edges[1][0] || edges[0][0] === edges[1][1])
+    return edges[0][0];
+  else if (edges[0][1] === edges[1][0] || edges[0][1] === edges[1][1])
+    return edges[0][1];
+};
