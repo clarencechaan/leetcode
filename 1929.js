c7d26b322c5936d75fa127b5af7a5222b7f0cf23
Clarence Chan <clarencechaan@icloud.com>
2023-02-01 11:32:58 -0500

    problem 1929

---
+++ 1929.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var getConcatenation = function (nums) {
+  // return [...nums, ...nums];
+
+  let ans = [];
+  for (let i = 0; i < nums.length; i++) ans.push(nums[i]);
+  for (let i = 0; i < nums.length; i++) ans.push(nums[i]);
+  return ans;
+};
