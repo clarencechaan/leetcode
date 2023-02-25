cf68f3ed4df9509c498100ef54faba998bc0730d
Clarence Chan <clarencechaan@icloud.com>
2023-02-25 10:49:42 -0500

    problem 2427

---
+++ easy/2427.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} a
+ * @param {number} b
+ * @return {number}
+ */
+var commonFactors = function (a, b) {
+  let result = 0;
+  for (let i = 1; i <= Math.min(a, b); i++)
+    if (a % i === 0 && b % i === 0) result++;
+  return result;
+};
