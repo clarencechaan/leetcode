0fd5593e96c4728bda2eae3aa917a8be01799660
Clarence Chan <clarencechaan@icloud.com>
2023-04-28 10:15:45 -0400

    problem 951

---
+++ medium/951.js
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root1
+ * @param {TreeNode} root2
+ * @return {boolean}
+ */
+var flipEquiv = function (root1, root2) {
+  if (!root1 && !root2) return true;
+  if (root1?.val !== root2?.val) return false;
+  if (root1.left?.val !== root2.left?.val)
+    [root2.left, root2.right] = [root2.right, root2.left];
+  return (
+    flipEquiv(root1.left, root2.left) && flipEquiv(root1.right, root2.right)
+  );
+};
