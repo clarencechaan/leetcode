8acb50b2a40bc4c85dbaa71a09910727aac1572a
Clarence Chan <clarencechaan@icloud.com>
2023-06-29 10:15:10 -0400

    problem 1381

---
+++ medium/1381.js
@@ -0,0 +1,40 @@
+/**
+ * @param {number} maxSize
+ */
+var CustomStack = function (maxSize) {
+  this.stack = [];
+  this.maxSize = maxSize;
+};
+
+/**
+ * @param {number} x
+ * @return {void}
+ */
+CustomStack.prototype.push = function (x) {
+  if (this.stack.length < this.maxSize) this.stack.push(x);
+};
+
+/**
+ * @return {number}
+ */
+CustomStack.prototype.pop = function () {
+  if (this.stack.length) return this.stack.pop();
+  return -1;
+};
+
+/**
+ * @param {number} k
+ * @param {number} val
+ * @return {void}
+ */
+CustomStack.prototype.increment = function (k, val) {
+  for (let i = 0; i < Math.min(k, this.stack.length); i++) this.stack[i] += val;
+};
+
+/**
+ * Your CustomStack object will be instantiated and called as such:
+ * var obj = new CustomStack(maxSize)
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * obj.increment(k,val)
+ */
