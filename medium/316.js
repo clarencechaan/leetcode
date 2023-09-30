782ff81d757d7efbce8393b6a49dcee7dfef5614
Clarence Chan <clarencechaan@icloud.com>
2023-09-30 11:25:54 -0400

    problem 316

---
+++ medium/316.js
@@ -0,0 +1,34 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var removeDuplicateLetters = function (s) {
+  let distinct = new Set(s.split("").sort());
+
+  function substringContainsSet(idx, set) {
+    let remaining = new Set(set);
+    for (let i = idx; i < s.length; i++) {
+      remaining.delete(s[i]);
+      if (!remaining.size) break;
+    }
+    return !remaining.size;
+  }
+
+  function recurse(idx = 0) {
+    if (!distinct.size) return "";
+    for (const char of distinct) {
+      let charIdx = -1;
+      for (let i = idx; i < s.length; i++)
+        if (s[i] === char) {
+          charIdx = i;
+          break;
+        }
+      if (charIdx >= 0 && substringContainsSet(charIdx, distinct, char)) {
+        distinct.delete(char);
+        return char + recurse(charIdx + 1);
+      }
+    }
+  }
+
+  return recurse();
+};
