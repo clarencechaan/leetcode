9e26334f4caa41c98a5f813c387fc2cb7d795cc6
Clarence Chan <clarencechaan@icloud.com>
2024-02-29 18:01:26 -0500

    problem 2390

---
+++ medium/2390.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var removeStars = function (s) {
+  let arr = [];
+  for (let i = 0; i < s.length; i++)
+    if (s[i] !== "*") arr.push(s[i]);
+    else arr.pop();
+  return arr.join("");
+};
+
+console.log(removeStars("leet**cod*e"));
